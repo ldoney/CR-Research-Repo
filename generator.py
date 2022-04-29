8307e04fb35ed828093f9f1a185020525351f018
Lincoln Doney <lincolndoney@outlook.com>
2022-04-29 14:05:36 -0400

    Stable

---
+++ generator.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python3 
+# -*- coding: utf-8 -*- 
+
+""" 
+Created on Sun Jun 28 20:30:43 2020 
+Authors Deven Bowman, Julia Codere 
+""" 
+def printf(format, *values):
+    print(format % values )
+   
+import numpy as np 
+import matplotlib.pyplot as plt 
+import re
+from matplotlib.pyplot import cm
+import itertools
+
+STYLES = [
+    ('red', '.'),
+    ('black', 's'),
+    ('blue', 'o'),
+    ('orange', '+'),
+    ('pink', 'h'),
+    ('cadetblue', '*'),
+    ('green', 'D'),
+    ('gray', '1'),
+    ('darkgray', 'v')
+]
+
+plt.rcParams["font.weight"] = "bold" #makes the font on the plots bold 
+plt.rcParams["axes.labelweight"] = "bold" #makes the fonts on the plots bold 
+
+## Arrays that will hold info about each data set 
+
+#contains -999 just to initialize the array 
+
+fileNames=[] 
+massNum=[] 
+chrgNum=[] 
+color=[] 
+markerSize=[] 
+markerSymbol=[] 
+fillvalue=[] 
+computeBins=[] 
+
+## Determines what power E is raised to 
+
+POWER = 2.75 
+
+plt.figure(figsize=(19, 8), dpi=80)
+
+plt.rcParams["figure.figsize"] = plt.rcParamsDefault["figure.figsize"]
+
+##plotInput(name of the file, atomic mass number, charge number, color for plot, size of the marker, type of marker) 
+## If the file containes energy data, enter None for MassNum and ChrgNum 
+def plotInput(FileName, MassNum, ChrgNum, colr, mrkerSize, mrkerSymbol, fillValue, bins=True):
+    ## adds information to each array 
+    fileNames.append(FileName) 
+    massNum.append(MassNum) 
+    chrgNum.append(ChrgNum) 
+    color.append(colr) 
+    markerSize.append(mrkerSize) 
+    markerSymbol.append(mrkerSymbol) 
+    fillvalue.append(fillValue)
+    computeBins.append(bins)
+
+element_table = {}
+## data will plot in the order that plotInput is called 
+# data = np.genfromtxt('Compileflux_New_Set_data.csv', delimiter=',', skip_header=2,usecols=np.arange(0,35)) ## Opens the csv file of the form 'He_AMS_2017.csv' and skips the header 
+MATCHER = re.compile(r",(?=(?:[^\"']*[\"'][^\"']*[\"'])*[^\"']*$)")
+
+data = [MATCHER.split(line) for line in open('Compileflux_New_Set_data.csv')][2:]
+
+for row in data:
+    element = row[3]
+    if(element_table.get(element) == None):
+        element_table[element] = []
+    row = [None if v == -9999 else v for v in row]
+    element_table[element].append(row)
+    
+''' 
+### Plotting Loop ### 
+-runs for each file, calculating flux and error bars 
+-if rigidity data is entered, the energy conversion is calculated 
+''' 
+
+#plt.figure(dpi=5000) 
+ignore = []
+to_plot = element_table["e+"]
+experiments = {}
+
+for row in to_plot:
+    name = row[2]
+    element = row[3]
+    year = row[4]
+    paper = row[5]
+    A = None
+    Z = None
+
+    ## Assumes that None input for mass number and charge means the data is in terms of energy 
+    Is_Energy = ( A == None or Z == None ) 
+    ## ENERGY CSV ## 
+
+    if Is_Energy: 
+        E1 = float(row[15])
+        E2 = float(row[16])
+        if False:
+            E = 10**((np.log10(E1)+np.log10(E2))/2) #computes center of energy bin 
+        else: 
+            E = float(row[17])
+            
+        error_pos = float(row[18])
+        error_neg = float(row[19])
+        F = float(row[20])
+
+        if(experiments.get(name) == None):
+            experiments[name] = []
+        experiments[name].append((E, F, error_pos, error_neg, element, name.replace('/', u"\u2215"), year))
+        
+            
+
+    ## Plots data with file specific formatting ##
+for k in experiments.keys():
+    v = experiments[k]
+    new_rc = []
+    for datapoint in v:
+        E = datapoint[0]
+        F = datapoint[1]
+        error_pos = datapoint[2]
+        error_neg = datapoint[3]
+        new_rc.append([E, F, error_pos, error_neg])
+    
+    experiments[k] = (np.array(new_rc),datapoint[4],datapoint[5],datapoint[6])
+
+if(len(experiments.keys()) > len(STYLES)):
+    RAINBOW = iter(cm.rainbow(np.linspace(0, 1, len(experiments.keys()) - len(STYLES))))
+
+MARKER_ITER = itertools.cycle(('.', 's', 'o', '+', 'h', '*', 'D', '1', 'v'))
+
+i = 0
+for k in experiments.keys():
+    if(k in ignore):
+        continue
+    
+    r = experiments[k]
+    d = r[0]
+    E = d[:,0:1].reshape(-1)
+    F = d[:,1:2].reshape(-1)
+    error_pos = d[:,2:3].reshape(-1)
+    error_neg = d[:,3:4].reshape(-1)
+    np.savetxt('e+_data/%s_%s_%s.csv'%(r[1],r[2],r[3]), d, delimiter=",", header="E,F,err_pos,err_neg",comments='')
+    if(i < len(STYLES)):
+        color = STYLES[i][0]
+        marker = STYLES[i][1]
+    else:
+        color = next(RAINBOW)
+        marker = next(MARKER_ITER)
+
+    error_pos=(error_pos*E**POWER) #computes plotted error amounts 
+    error_neg=(error_neg*E**POWER) 
+    E_err = np.array(list(zip(error_pos, error_neg))).T
+
+    halving = []
+    for g,err in enumerate(error_neg):
+        if(err/(F[g]*E[g]**POWER) > 0.9):
+            error_neg = error_neg/4
+
+    # This is sort of jank, let's see how it goes
+    for g,err in enumerate(error_pos):
+        if(err/(F[g]*E[g]**POWER) > 0.9):
+            printf("Warning: Err on %s busted! %f > %f", r[2], err, F[g]*E[g]**POWER)
+            ignore.append(k)
+            break
+
+    # if(not(k in ignore)):
+    plt.plot(E, (F*E**POWER), marker, ms=4, c=color, label=k) ##plots the data 
+    plt.errorbar(E, (F*E**POWER), yerr=E_err, fmt=' ', ecolor=color) ##plots error bars 
+    
+    i = i + 1
+
+## Makes the plot log-log ##     
+plt.xscale('log') 
+plt.yscale('log') 
+
+## Sets axes limits ## 
+## plt.ylim(10**3, 10**5)
+## plt.xlim(10**.75, 10**7.21)
+
+## Adds Plots Labels/Title ## 
+plt.title('Flux vs Energy: %s'%(element), fontweight='bold', fontsize=18) 
+plt.xlabel('Energy/nucleon (GeV/n)', fontsize = 15) 
+
+#Make sure the power in the label is correct 
+plt.ylabel(r'Φ E$^{%s}$(m$^{-2}$s$^{-1}$sr$^{-1}$GeV/n$^{1.75}$)'%(POWER), fontsize =14) 
+
+## Formats axis tick marks ## 
+plt.tick_params(axis='both',which = 'both', direction = 'in', length= 3, width = 1.2 ) #adjust all axis ticks to point inward 
+plt.tick_params(axis='both', direction = 'in', length = 5, labelsize=12 ) #further adjust size of only major tick marks 
+ 
+# plt.legend(loc="right", bbox_to_anchor=(2.0, 0.5), borderaxespad=0) 
+
+plt.legend(bbox_to_anchor=(1.02, 1.1), loc='upper left', borderaxespad=0)
+plt.savefig("e+_plot.png")
+plt.savefig("e+_plot.jpg")
+plt.show()
---
+++ plot.py
@@ -0,0 +1,232 @@
+#!/usr/bin/env python3 
+# -*- coding: utf-8 -*- 
+
+""" 
+Created on Sun Jun 28 20:30:43 2020 
+Authors Deven Bowman, Julia Codere 
+""" 
+def printf(format, *values):
+    print(format % values )
+
+import numpy as np 
+import matplotlib.pyplot as plt 
+from matplotlib.pyplot import cm
+import re
+
+def flatten_list(_2d_list):
+    flat_list = []
+    # Iterate through the outer list
+    for element in _2d_list:
+        if type(element) is list:
+            # If the element is of type list, iterate through the sublist
+            for item in element:
+                flat_list.append(item)
+        else:
+            flat_list.append(element)
+    return flat_list
+
+STYLES = [
+    ('red', '.'),
+    ('black', 's'),
+    ('blue', 'o'),
+    ('orange', '+'),
+    ('pink', 'h'),
+    ('cadetblue', '*'),
+    ('green', 'D'),
+    ('gray', '1'),
+    ('darkgray', 'v')
+]
+
+plt.rcParams["font.weight"] = "bold" #makes the font on the plots bold 
+plt.rcParams["axes.labelweight"] = "bold" #makes the fonts on the plots bold 
+
+## Arrays that will hold info about each data set 
+
+fileNames=[] 
+massNum=[] 
+chrgNum=[] 
+color=[] 
+markerSize=[] 
+markerSymbol=[] 
+fillvalue=[] 
+computeBins=[] 
+
+## Determines what power E is raised to 
+
+POWER = 2.75 
+
+##plotInput(name of the file, atomic mass number, charge number, color for plot, size of the marker, type of marker) 
+## If the file containes energy data, enter None for MassNum and ChrgNum 
+def plotInput(FileName, MassNum, ChrgNum, colr, mrkerSize, mrkerSymbol, fillValue, bins=True):
+    ## adds information to each array 
+    fileNames.append(FileName) 
+    massNum.append(MassNum) 
+    chrgNum.append(ChrgNum) 
+    color.append(colr) 
+    markerSize.append(mrkerSize) 
+    markerSymbol.append(mrkerSymbol) 
+    fillvalue.append(fillValue)
+    computeBins.append(bins)
+
+## data will plot in the order that plotInput is called 
+
+element = "e+"
+if(element == "P"):
+    plotInput('P_data/P_ACE-CRIS(1997-1998)_2013', None, None,'red', 4,'s', 'full') #point markers  
+    plotInput('P_data/P_ALICE(1987)_1992', None, None,'black', 4,'D', 'full') #point markers  
+    plotInput('P_data/P_ACE-CRIS(2001-2003)_2013', None, None,'blue', 4,'.', 'full') #point markers  
+    plotInput('P_data/P_ACE-CRIS(2009-2010)_2013', None, None,'goldenrod', 4,'o', 'full') #point markers  
+    plotInput('P_data/P_Balloon(1971-1972)_1974', None, None,'pink', 4,'v', 'full') #point markers  
+    plotInput('P_data/P_Balloon(1968)_1979', None, None,'cadetblue', 4,'*', 'full') #point markers  
+    plotInput('P_data/P_CRISIS(1977)_1981', None, None,'orange', 4,'^', 'full') #point markers  
+    plotInput('P_data/P_HEAO3-C2(1979-1980)_1990', None, None,'green', 4,'P', 'full') #point markers  
+    plotInput('P_data/P_Voyager1-HET-Aend(2012-2015)_2016', None, None,'cadetblue', 4,'*', 'full') #point markers  
+elif (element == "S"):
+    plotInput('S_data/S_ACE-CRIS_2013', None, None,'red', 4,'s', 'full', bins=False) #point markers  
+    plotInput('S_data/S_ALICE_1992', None, None,'black', 4,'.', 'full', bins=False) #point markers  
+    plotInput('S_data/S_Balloon-EWAsym_1997', None, None,'blue', 4,'o', 'full', bins=False) #point markers  
+    plotInput('S_data/S_Balloon-OpeningAngle_1997', None, None,'gray', 4,'v', 'full', bins=False) #point markers  
+    plotInput('S_data/S_Balloon(1974)_1974', None, None,'pink', 4,'^', 'full', bins=False) #point markers  
+    plotInput('S_data/S_Balloon(1978)_1978', None, None,'orange', 4,'1', 'full', bins=False) #point markers  
+
+    # This one is messed up, ask Sasa
+#    plotInput('S_data/S_Balloon(1993)_1993', None, None,'green', 4,'d', 'full', bins=False) #point markers  
+
+    plotInput('S_data/S_CRISIS_1981', None, None,'cadetblue', 4,'8', 'full', bins=False) #point markers  
+    plotInput('S_data/S_HEAO3-C2_1990', None, None,'magenta', 4,'.', 'full', bins=False) #point markers  
+    plotInput('S_data/S_TRACER03_2008', None, None,'brown', 4,'p', 'full', bins=False) #point markers  
+    plotInput('S_data/S_Voyager1-HET-Aend_2016', None, None,'lightblue', 4,'P', 'full', bins=False) #point markers  
+    plotInput('S_data/S_Voyager1-HET-Bend_2016', None, None,'lightgreen', 4,'*', 'full', bins=False) #point markers  
+    plotInput('S_data/S_Voyager1-LET_2016', None, None,'goldenrod', 4,'D', 'full', bins=False) #point markers  
+else:
+    MATCHER = re.compile(r",(?=(?:[^\"']*[\"'][^\"']*[\"'])*[^\"']*$)")
+    data = flatten_list([(MATCHER.split(line)) for line in open('e+_data/files')])
+    data = map(lambda d: d.replace(".csv", "").replace("\n","").replace(" ", ""),data)
+    data = list(filter(lambda d: d != '',data))
+    if(len(data) > len(STYLES)):
+        RAINBOW = iter(cm.rainbow(np.linspace(0, 1, len(data) - len(STYLES))))
+    for i,f in enumerate(data):
+        if(i < len(STYLES)):
+            c = STYLES[i][0]
+            marker = STYLES[i][1]
+        else:
+            c = next(RAINBOW)
+            marker = "s" # we'll replace this later
+        plotInput('e+_data/%s'%(f.replace(".csv", "").replace("\n","").replace(" ", "")), None, None, c, 4,marker, 'full', bins=False) #point markers  
+
+''' 
+### Plotting Loop ### 
+-runs for each file, calculating flux and error bars 
+-if rigidity data is entered, the energy conversion is calculated 
+''' 
+
+#plt.figure(dpi=2000) 
+i=0 
+ignore = []
+
+## figure_name = plt.figure(figsize=(9,7)) 
+while i < len(fileNames): 
+   ### accesses the file specific info ### 
+    fileName= fileNames[i]
+    name = fileName.split('/')[-1].split('_')
+    element =name[0] 
+    paper =name[1] 
+    year = name[2] 
+    A = massNum[i] 
+    Z = chrgNum[i] 
+    fill = fillvalue[i] 
+
+    data = np.genfromtxt(fileName + '.csv', delimiter=',', skip_header=1) ## Opens the csv file of the form 'He_AMS_2017.csv' and skips the header 
+
+    # Fixes error with single row
+    if(data.ndim != 2):
+        data = data.reshape((1,data.shape[0]))
+
+    ## Assumes that None input for mass number and charge means the data is in terms of energy 
+    Is_Energy = ( A == None or Z == None ) 
+    ## ENERGY CSV ## 
+
+    if Is_Energy: 
+        # y = input('Do you need to calculate the mean values of the energy bins for ' + fileName + ', Y or N: ') 
+        y = "y"
+        if computeBins[i] == True:
+            E1 = data[:,0:1].reshape(-1) ## COLUMN 0 
+            E2 = data[:,1:2].reshape(-1) ## COLUMN 1 
+            F = data[:,2:3].reshape(-1) ## COLUMN 2 
+            error_pos = data[:,3:4].reshape(-1) ## column 3 
+            error_neg = data[:,4:5].reshape(-1) ## column 4 
+            E = 10**((np.log10(E1)+np.log10(E2))/2) #computes center of energy bin 
+        else: 
+            E = data[:,0:1].reshape(-1)
+            F = data[:,1:2].reshape(-1)
+            error_pos = data[:,2:3].reshape(-1)
+            error_neg = data[:,3:4].reshape(-1)
+
+        
+        error_pos=(error_pos*E**POWER) #computes plotted error amounts 
+        error_neg=(error_neg*E**POWER) 
+        for k,err in enumerate(error_neg):
+            if(err > F[k]):
+                print(data)
+                printf("Warning: Err on %s busted! %f > %f", paper, err, F[k])
+                ignore.append(paper)
+                # This is sort of jank, let's see how it goes
+                break
+        E_err = np.array(list(zip(error_pos, error_neg))).T #creates a 2D array of error that can be plotted 
+            
+    ## RIGIDITY CSV ## 
+    ## if a mass number and charge number were enterd, it treats the data as rigidity 
+    else: 
+            ## Calculation Information ## 
+            ratio = 931.5 ##ratio of MeV/c^2 per 1 u 
+            M = A*ratio/10**3 ##mass [GeV/c^2] 
+
+            ## Rigidity ## 
+            R1 = data[:,0:1] 
+            R2 = data[:,1:2] 
+            F_R = data[:,2:3] 
+            sys_err = data[:,3:4] 
+            stat_err = data[:,4:5] 
+            R_err = np.sqrt(stat_err**2 + sys_err**2) ## AMS Paper Error Calculation Method 
+            R = 10**((np.log10(R1)+np.log10(R2))/2) ##calculates center of rigidity bin(not used when plotting energy) 
+
+             ## Energy ## 
+            E1 = (np.sqrt(Z**2*R1**2+M**2)-M)/A # calculates energy from rigidity 
+            E2 = (np.sqrt(Z**2*R2**2+M**2)-M)/A 
+            E = 10**((np.log10(E1)+np.log10(E2))/2) ##calculates center of energy bin 
+            ##Calculates energy flux F 
+            dRdE = (R2-R1)/(E2-E1) 
+            F = F_R*dRdE 
+            E_err = ((R_err*dRdE)*E**POWER) ##calculates plotted Energy flux error 
+
+
+    if(not (paper in ignore)):
+        ## Plots data with file specific formatting ## 
+        plt.plot(E, (F*E**POWER), markerSymbol[i], ms=markerSize[i], c=color[i], label = paper, fillstyle=fill) ##plots the data 
+        plt.errorbar(E, (F*E**POWER), yerr=E_err, fmt=' ', ecolor=color[i]) ##plots error bars 
+
+    i+=1 ##moves on to next file     
+
+## Makes the plot log-log ##     
+plt.xscale('log') 
+plt.yscale('log') 
+
+## Sets axes limits ## 
+## plt.ylim(10**3, 10**5)
+## plt.xlim(10**.75, 10**7.21)
+
+## Adds Plots Labels/Title ## 
+plt.title('Flux vs Energy: %s'%(element), fontweight='bold', fontsize=18) 
+plt.xlabel('Energy/nucleon (GeV/n)', fontsize = 15) 
+
+#Make sure the power in the label is correct 
+plt.ylabel(r'Φ E$^{%s}$(m$^{-2}$s$^{-1}$sr$^{-1}$GeV/n$^{1.75}$)'%(POWER), fontsize =14) 
+
+## Formats axis tick marks ## 
+plt.tick_params(axis='both',which = 'both', direction = 'in', length= 3, width = 1.2 ) #adjust all axis ticks to point inward 
+plt.tick_params(axis='both', direction = 'in', length = 5, labelsize=12 ) #further adjust size of only major tick marks 
+ 
+plt.legend(loc="right", bbox_to_anchor=(1.8, 0.5), borderaxespad=0) 
+
+## plt.legend(loc=0) 
+plt.show()
